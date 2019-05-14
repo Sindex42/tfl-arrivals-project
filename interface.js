f2cac7ef8c668ac1ff654f6b52d1feb54bb73ac9
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-14 17:16:10 +0100

    Add upcoming nr 4 bus

--- interface.js
+++ interface.js
@@ -7,6 +7,7 @@ const canonburyStn = '910GCNNB' // Canonbury Overground
 const platformNr = 2 // Southbound to Crystal Palace/West Croydon
 
 const aberdeenBus = '490003053S' // Highbury Grove School / Aberdeen Park (CZ)
+const busLine = '4'
 
 window.onload = () => {
   $.getJSON(`${url}/${canonburyStn}/arrivals`, (data) => {
@@ -27,5 +28,25 @@ window.onload = () => {
     }
 
     document.getElementById('overground').innerHTML = message
+  })
+
+  $.getJSON(`${url}/${aberdeenBus}/arrivals`, (data) => {
+    let minExpected = 0
+    let message = ''
+
+    for (let i = 0; i < data.length; i++) {
+      var lineName = data[i].lineName
+      var expected = new Date(data[i].expectedArrival)
+      var timeToStation = data[i].timeToStation
+      console.log(lineName, toMinutes(timeToStation))
+
+      if (lineName !== busLine) { continue }
+      if (expected < minExpected || minExpected === 0) {
+        message = `Next ${busLine} Bus in ${toMinutes(timeToStation)} min`
+        minExpected = expected
+      }
+    }
+
+    document.getElementById('bus').innerHTML = message
   })
 }
